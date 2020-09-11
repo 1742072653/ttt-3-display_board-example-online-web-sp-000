794b57a8affe404ff2bf574f7f5c810ebb2e5a7c
Wenhao Guo <gwh1742072653@gmail.com>
2020-09-11 17:37:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "   |   |   \n"
-      expected_output += "-----------\n"
-      expected_output += "   |   |   \n"
-      expected_output += "-----------\n"
-      expected_output += "   |   |   \n"
+  puts "-----------\n"
+     
 end
